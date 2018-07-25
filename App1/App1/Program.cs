208a98ec56897a770228860806c5269bba66b703
Iain Foster <9925082@student.toiohomai.ac.nz>
2018-07-25 16:46:52 +1200

    first

---
+++ App1/App1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //outputs to the console.
+            Console.WriteLine("hello world");
+        }   //Console.readline();
+    }
+}
